21cb2368696b4c5c2a0e4516ef852b9876f0ea12
rndl47 <70772774+rndl47@users.noreply.github.com>
2020-11-11 18:21:41 +0300

    - Added important mathematical definitions.

--- definitions.h
+++ definitions.h
@@ -4,7 +4,7 @@
 #define M_E   2.7182818284590452354
 
 /* log_e 2 */
-#define M_LN2 0.69314718055994530942
+#define M_LN2	0.69314718055994530942
 
 /* log_e 10 */
 #define M_LN10   2.30258509299404568402
@@ -26,3 +26,21 @@
 
 /* sqrt(2) */
 #define M_SQRT2   1.41421356237309504880
+
+/* 1/pi */
+#define M_1_PI	0.318309886183790671538
+
+/* 2/pi */
+#define M_2_PI	0.636619772367581343076
+
+/* sqrt(M_PI) ~default pi (3.14) */
+#define	M_SQRTPI	1.77245385090551602729
+
+/* 1/sqrt(M_PI) ~take sqrt(M_PI) as 1.77 */
+#define	M_1_SQRTPI_1	0.05649717514124293785
+
+/* prototype 1/sqrt(M_PI) ~take sqrt(M_PI) as 1.77245385090551602729 */
+#define M_1SQRTPI_2	0.56418958354
+
+/* 2/sqrt(M_PI) ~take sqrt(M_PI) as 1.77245385090551602729 */
+#define	M_2_SQRTPI	1.12837916709551257390
