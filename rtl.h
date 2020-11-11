89ad49408dbe03bab3e1df43fc8660b49fce3660
rndl47 <70772774+rndl47@users.noreply.github.com>
2020-11-11 19:13:47 +0300

    - Refactoring code. - Rewrite rtl_exp.

--- rtl.h
+++ rtl.h
@@ -170,58 +170,35 @@ int32_t inline __fastcall rtl_abs(int32_t x)
     return i;
 }
 
-// method in polynomial interpolation and approximation theory.
-extern float inline __fastcall rtl_expf(const float x);
-float inline __fastcall rtl_expf(const float x)
+extern int inline __fastcall rtl_exp(const int x);
+int inline __fastcall rtl_exp(const int x)
 {
     // check was x == 0.
     if (x == 0) {
         return 0;
     }
 
-    // If x < -709 or x > 709, raises an assertion error.
+    // if x < -709 or x > 709 raises an assertion error.
     // NOTE rndl47: Coz we are using a multiplicative constant less than 12 quickly reduces the amount of accuracy you can obtain as you approach the bounds of.
     assert(-709 <= x && x <= 709);
 
-    float y = rtl_abs(x);
-    int k = rtl_cefl((y / M_LN2) - 0.5);
-    float r = y - (k * M_LN2);
+    // calculate the absolute number from x.
+    double abs_x = rtl_abs(x);
 
-    // coefficient vector.
-    std::vector<float> coefficients = {
-        1.000000000000000,
-        1.000000000000000,
-        0.500000000000002,
-        0.166666666666680,
-        0.041666666666727,
-        0.008333333333342,
-        0.001388888888388,
-        1.984126978734782e-4,
-        2.480158866546844e-5,
-        2.755734045527853e-6,
-        2.755715675968011e-7,
-        2.504861486483735e-8,
-        2.088459690899721e-9,
-        1.632461784798319e-10 };
+    // magic number.
+    double tn = 1.0;
 
-    // number taylor.
-    float nt = 1.143364767943110e-11;
-
-    // main part.
-    for (auto i = coefficients.rbegin(); i != coefficients.rend(); i++)
-    {
-        nt = nt * r + *i;
+    // chose a truncation point.
+    int point = (int)rtl_cefl(abs_x * M_E) * 12;
+    for (int i = point; i > 0; i--) {
+        tn = tn * (abs_x / i) + 1.0;
     }
 
-    // exp.
-    nt *= exp2(k);
-
-    // If the original input x is less than 0.
-    if (x < 0)
-    {
-        return 1 / nt;
+    // if the original input x is less than 0.
+    if (x < 0) {
+        tn = 1 / tn;
     }
 
     // return result.
-    return nt;
+    return tn;
 }
